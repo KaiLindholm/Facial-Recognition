2943f92527c3958637d62dd394b45cdf5d307282
KaiLindholm <42724481+KaiLindholm@users.noreply.github.com>
2022-09-27 18:27:55 -0500

    Added real-time camera view

--- face_detection.py
+++ face_detection.py
@@ -8,12 +8,12 @@ def detect_faces(img, draw_box=True):
     faces = haar.detectMultiScale(grayscale_img, scaleFactor=1.6)
     
     # draw bounding box 
-    for(x,y, width, height) in faces:
+    for x, y, width, height in faces:
         if draw_box: 
             cv2.rectangle(img, (x,y), (x+width, y+height), (0,255,0),5) # draw a bounding box starting at x,y with color red
-    face_box = img[y:y+height, x:x+width]
-    face_coords = [x,y, width, height]
-    return img, face_box, face_coords
+    #face_box = img[y:y+height, x:x+width]
+    #face_coords = [x,y, width, height]
+    return img#, face_box, face_coords
 
 
 # test detection 
@@ -27,4 +27,28 @@ def detect_faces_test():
         detected_face, _ , _ = detect_faces(img)
         cv2.imwrite('faces/detected/' + image, detected_face)
 
-detect_faces_test()
+#detect_faces_test()
+#TODO Multithreading 
+def video_capture():
+    # define a video capture object
+    vid = cv2.VideoCapture(0)
+    vid.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+    vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+    
+    z = 0
+    while(True):
+        ret, frame = vid.read()
+        test_frame = detect_faces(frame)
+        # Display the resulting frame
+        cv2.imshow('frame', test_frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
+video_capture()
+    
+    
